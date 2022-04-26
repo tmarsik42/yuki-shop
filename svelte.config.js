3fda995c42c05be43dd0fae6712ce9fc1bf6cb68
Tadeáš Maršík <tadeas.marsik@teta.eu>
2022-04-26 18:20:44 +0200

    adapter vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import node from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: node()
+		adapter: vercel()
 	}
 };
 
