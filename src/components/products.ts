21edce415c7eb46498e87dd39258c555902de209
Tadeáš Maršík <tadeas.marsik@teta.eu>
2022-04-30 15:31:38 +0200

    irl items

--- src/components/products.ts
+++ src/components/products.ts
@@ -4,196 +4,51 @@ export interface Product {
 	name: string;
 	description: string;
 	price: number;
-	imageUrl: string;
+	imgCount: number;
 }
 
 const skirts: Product[] = [
 	{
-		id: '76d1bfa5-d4c7-407c-9381-f146112e600c',
-		category: 'skirts',
-		name: 'Skirt 1',
-		description: 'Lorem ipsum description of Skirt 1...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
+		id: 'pastel-hearts',
+		category: 'clothing',
+		name: 'Pastel Hearts',
+		description: '',
+		price: 1200,
+		imgCount: 2
 	},
 	{
-		id: '35e9da8b-3217-4a30-811e-8f42376036bd',
-		category: 'skirts',
-		name: 'Skirt 2',
-		description: 'Lorem ipsum description of Skirt 2...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '6cd9b8a1-77bb-424b-8e55-19799d89552f',
-		category: 'skirts',
-		name: 'Skirt 3',
-		description: 'Lorem ipsum description of Skirt 3...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '9edaf3c9-c407-42ff-a87d-51e94aa9e48e',
-		category: 'skirts',
-		name: 'Skirt 4',
-		description: 'Lorem ipsum description of Skirt 4...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '4b970e52-5a25-4827-b76c-38636e46aba0',
-		category: 'skirts',
-		name: 'Skirt 5',
-		description: 'Lorem ipsum description of Skirt 5...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: 'ef7e33c4-85f9-4b62-8458-ca824798569d',
-		category: 'skirts',
-		name: 'Skirt 6',
-		description: 'Lorem ipsum description of Skirt 6...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '6d62f457-f1c1-49b4-8309-3883eda1df4b',
-		category: 'skirts',
-		name: 'Skirt 7',
-		description: 'Lorem ipsum description of Skirt 7...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: 'a9d53769-84f2-48e0-b798-595a8ca197a6',
-		category: 'skirts',
-		name: 'Skirt 8',
-		description: 'Lorem ipsum description of Skirt 8...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: 'bd8909a4-ad97-4f73-bf0c-714dad59db9e',
-		category: 'skirts',
-		name: 'Skirt 9',
-		description: 'Lorem ipsum description of Skirt 9...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: 'cfe544f0-48c5-4601-a486-187e001016d8',
-		category: 'skirts',
-		name: 'Skirt 10',
-		description: 'Lorem ipsum description of Skirt 10...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '84b3bfcc-c37a-42d2-9624-d22c38e8e79e',
-		category: 'skirts',
-		name: 'Skirt 11',
-		description: 'Lorem ipsum description of Skirt 11...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '9b8ad497-23a9-4212-9b27-df3033a25f40',
-		category: 'skirts',
-		name: 'Skirt 12',
-		description: 'Lorem ipsum description of Skirt 12...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
+		id: 'spring-welcome',
+		category: 'clothing',
+		name: 'Spring Welcome',
+		description: '',
+		price: 1200,
+		imgCount: 4
 	}
 ];
 
 const original: Product[] = [
 	{
-		id: 'e5578f2f-b5ea-4382-ade7-aae9b219a3e0',
-		category: 'original',
-		name: 'original 1',
-		description: 'Lorem ipsum description of original 1...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '657dffaa-f63d-4c3e-9118-3e5cdacaeb0a',
-		category: 'original',
-		name: 'original 2',
-		description: 'Lorem ipsum description of original 2...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '0de66b61-ba17-421a-bbd8-6f18768101b4',
-		category: 'original',
-		name: 'original 3',
-		description: 'Lorem ipsum description of original 3...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '8af61c1f-42e6-4369-8743-45bd6b690a0d',
-		category: 'original',
-		name: 'original 4',
-		description: 'Lorem ipsum description of original 4...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
+		id: 'annas-rose-garden',
+		category: 'clothing',
+		name: "Anna's Rose Garden",
+		description: '',
+		price: 2300,
+		imgCount: 3
 	}
 ];
 
 const accessories: Product[] = [
+	/*
 	{
 		id: 'ac39e5d4-596e-4a75-ab00-1582374b606f',
 		category: 'accessories',
 		name: 'accessory 1',
 		description: 'Lorem ipsum description of accessory 1...',
 		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '7c96133f-2978-4c27-9124-5a0d8652daa3',
-		category: 'accessories',
-		name: 'accessory 2',
-		description: 'Lorem ipsum description of accessory 2...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '1cd48413-6954-46ee-b093-ac64d7553b73',
-		category: 'accessories',
-		name: 'accessory 3',
-		description: 'Lorem ipsum description of accessory 3...',
-		price: 2000,
-		imageUrl:
-			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
-	},
-	{
-		id: '119d0aba-39e5-4709-a64f-e0bf56d92eeb',
-		category: 'accessories',
-		name: 'accessory 4',
-		description: 'Lorem ipsum description of accessory 4...',
-		price: 2000,
-		imageUrl:
+		url:
 			'https://shahpourpouyan.com/wp-content/uploads/2018/10/orionthemes-placeholder-image-1.png'
 	}
+ 	*/
 ];
 
 const categories = new Map();
@@ -210,17 +65,11 @@ export function getAll() {
 }
 
 export function getClothingCategories() {
-	return Array.from(clothingCategories.keys());
+	return clothingCategories;
 }
 
-export function getCategory(chosenCategories) {
-	if (Array.isArray(chosenCategories)) {
-		const tmp = [];
-		chosenCategories.forEach((v) => tmp.push(findCategory(v)));
-		return tmp;
-	} else {
-		return findCategory(chosenCategories);
-	}
+export function getCategory(chosenCategory) {
+	return findCategory(chosenCategory);
 
 	function findCategory(c) {
 		if (categories.has(c)) {
@@ -231,4 +80,8 @@ export function getCategory(chosenCategories) {
 	}
 }
 
-getCategory(['skirts', 'original']);
+export function getProduct(id) {
+	let tmpArr = [];
+	getAll().forEach((c) => tmpArr.push(c.filter((item) => item.id === id)));
+	return tmpArr.filter((v) => v.length)?.flat()[0] ?? 'Zboží nenalezeno';
+}
