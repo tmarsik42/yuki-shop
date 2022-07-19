73523f32fff9a996d6a9f392a8bf1136e85c8bbb
Tadeáš Maršík <tadeas.marsik@teta.eu>
2022-07-19 18:50:48 +0200

    update dependencies

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+const config = defineConfig({
+	plugins: [sveltekit()],
+	server: {
+		port: 3000
+	}
+});
+
+export default config;
